5b382b879fdfa73c0b7a27125a5fcdab187ea91d
mcclurgm <mcgearboulder@gmail.com>
2019-02-14 17:29:41 -0600

    Started day 16 openGL, black screen

--- 310vector.c
+++ 310vector.c
@@ -5,7 +5,7 @@ Michael McClurg and Vermilion Villarreal
 ***/
 
 /* Copies the dim-dimensional vector v to the dim-dimensional vector copy. */
-void vecCopy(int dim, const double v[], double copy[]) {
+void vecCopy(int dim, const GLdouble v[], GLdouble copy[]) {
 	int i;
 	for(i = 0; i < dim; i++) {
 		copy[i] = v[i];
@@ -13,7 +13,7 @@ void vecCopy(int dim, const double v[], double copy[]) {
 }
 
 /* Adds the dim-dimensional vectors v and w. */
-void vecAdd(int dim, const double v[], const double w[], double vPlusW[]) {
+void vecAdd(int dim, const GLdouble v[], const GLdouble w[], GLdouble vPlusW[]) {
 	int i;
 	for(i = 0; i < dim; i++) {
 		vPlusW[i] = v[i] + w[i];
@@ -21,8 +21,8 @@ void vecAdd(int dim, const double v[], const double w[], double vPlusW[]) {
 }
 
 /* Subtracts the dim-dimensional vectors v and w. */
-void vecSubtract(int dim, const double v[], const double w[],
-		double vMinusW[]) {
+void vecSubtract(int dim, const GLdouble v[], const GLdouble w[],
+		GLdouble vMinusW[]) {
 	int i;
 	for(i = 0; i < dim; i++) {
 		vMinusW[i] = v[i] - w[i];
@@ -30,7 +30,7 @@ void vecSubtract(int dim, const double v[], const double w[],
 }
 
 /* Scales the dim-dimensional vector w by the number c. */
-void vecScale(int dim, double c, const double w[], double cTimesW[]) {
+void vecScale(int dim, GLdouble c, const GLdouble w[], GLdouble cTimesW[]) {
 	int i;
 	for(i = 0; i < dim; i++) {
 		cTimesW[i] = c * w[i];
@@ -43,21 +43,21 @@ void vecScale(int dim, double c, const double w[], double cTimesW[]) {
 /* By the way, there is a way to write a single vecSet function that works in
 all dimensions. The module stdarg.h, which is part of the C standard library,
 lets you write variable-arity functions. The general vecSet would look like
-	void vecSet(int dim, double a[], ...)
+	void vecSet(int dim, GLdouble a[], ...)
 where the '...' represents dim numbers to be loaded into a. We're not going to
 take this approach for two reasons. First, I try not to burden you with
 learning a lot of C that isn't strictly necessary. Second, the variable-arity
 feature is a bit dangerous, in that it provides no type checking. */
 
 /* Copies three numbers into a three-dimensional vector. */
-void vec3Set(double a0, double a1, double a2, double a[3]) {
+void vec3Set(GLdouble a0, GLdouble a1, GLdouble a2, GLdouble a[3]) {
 	a[0] = a0;
 	a[1] = a1;
 	a[2] = a2;
 }
 
 /* Copies four numbers into a four-dimensional vector. */
-void vec4Set(double a0, double a1, double a2, double a3, double a[4]) {
+void vec4Set(GLdouble a0, GLdouble a1, GLdouble a2, GLdouble a3, GLdouble a[4]) {
 	a[0] = a0;
 	a[1] = a1;
 	a[2] = a2;
@@ -65,8 +65,8 @@ void vec4Set(double a0, double a1, double a2, double a3, double a[4]) {
 }
 
 /* Copies eight numbers into a eight-dimensional vector. */
-void vec8Set(double a0, double a1, double a2, double a3, double a4, double a5,
-		double a6, double a7, double a[8]) {
+void vec8Set(GLdouble a0, GLdouble a1, GLdouble a2, GLdouble a3, GLdouble a4, GLdouble a5,
+		GLdouble a6, GLdouble a7, GLdouble a[8]) {
 	a[0] = a0;
 	a[1] = a1;
 	a[2] = a2;
@@ -78,9 +78,9 @@ void vec8Set(double a0, double a1, double a2, double a3, double a4, double a5,
 }
 
 /* Returns the dot product of the vectors v and w. */
-double vecDot(int dim, const double v[], const double w[]) {
+GLdouble vecDot(int dim, const GLdouble v[], const GLdouble w[]) {
     int i;
-	double total = 0;
+	GLdouble total = 0;
     for(i = 0; i < dim; i++) {
         total += v[i] * w[i];
     }
@@ -89,16 +89,16 @@ double vecDot(int dim, const double v[], const double w[]) {
 }
 
 /* Returns the length of the vector v. */
-double vecLength(int dim, const double v[]) {
-    double mag2 = vecDot(dim, v, v);
+GLdouble vecLength(int dim, const GLdouble v[]) {
+    GLdouble mag2 = vecDot(dim, v, v);
     return sqrt(mag2);
 }
 
 /* Returns the length of the vector v. If the length is non-zero, then also
 places a normalized (length-1) version of v into unit. The output can safely
 alias the input. */
-double vecUnit(int dim, const double v[], double unit[]) {
-    double mag = vecLength(dim, v);
+GLdouble vecUnit(int dim, const GLdouble v[], GLdouble unit[]) {
+    GLdouble mag = vecLength(dim, v);
 
     if(mag != 0.0) {
         int i;
@@ -112,7 +112,7 @@ double vecUnit(int dim, const double v[], double unit[]) {
 
 /* Computes the cross product of v and w, and places it into vCrossW. The
 output CANNOT safely alias the input. */
-void vec3Cross(const double v[3], const double w[3], double vCrossW[3]) {
+void vec3Cross(const GLdouble v[3], const GLdouble w[3], GLdouble vCrossW[3]) {
     vCrossW[0] = (v[1] * w[2]) - (v[2] * w[1]);
     vCrossW[1] = (v[2] * w[0]) - (v[0] * w[2]);
     vCrossW[2] = (v[0] * w[1]) - (v[1] * w[0]);
@@ -121,7 +121,7 @@ void vec3Cross(const double v[3], const double w[3], double vCrossW[3]) {
 /* Computes the vector v from its spherical coordinates. rho >= 0.0 is the
 radius. 0 <= phi <= pi is the co-latitude. -pi <= theta <= pi is the longitude
 or azimuth. */
-void vec3Spherical(double rho, double phi, double theta, double v[3]) {
+void vec3Spherical(GLdouble rho, GLdouble phi, GLdouble theta, GLdouble v[3]) {
     v[0] = rho * sin(phi) * cos(theta);
     v[1] = rho * sin(phi) * sin(theta);
     v[2] = rho * cos(phi);
